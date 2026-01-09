7b6b65e979e4b94128116b7d9a6998373858ffad
SebastianFernandes <sebastian@puna.studio>
2026-01-08 21:57:17 -0300

    init

---
+++ src/app/layout.tsx
@@ -0,0 +1,30 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+
+const inter = Inter({
+  subsets: ["latin"],
+  variable: "--font-inter",
+});
+
+export const metadata: Metadata = {
+  title: "Mr Bet",
+  description: "Mr Bet es una plataforma de apuestas online que te permite jugar a tus juegos favoritos de forma segura y fácil.",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <head>
+        <link rel="icon" href="/favicon.ico" />
+      </head>
+      <body className={inter.className}>
+        {children}
+      </body>
+    </html>
+  );
+}
